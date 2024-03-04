3c6ac63c04f10ac230ca5ec4a61818f2e0426b1b
bilal <bilalelsayed97@gmail.com>
2024-03-04 09:49:53 +0200

    codable plist as db

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -2,23 +2,20 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    //    var itemArray = ["Complete Local Database","Learn Almofire","Complete App with VIPER"]
-    var itemsList = [
-        Item("Complete Local Database"),
-        Item("Learn Almofire"),
-        Item("Complete App with VIPER"),
-        Item("Completed ?"),
-        Item("You are now an iOS Developer 🥵✅")
-    ]
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appending(path: "Items.plist")
     
-    let defaults = UserDefaults.standard
+    var itemsList: [Item] = []
+    
+    //    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if let dataFromDB = defaults.object(forKey: "ToDoListArray") as? [Item] {
-            itemsList = dataFromDB
-        }
         
+        //        if let dataFromDB = defaults.object(forKey: "ToDoListArray") as? [Item] {
+        //            itemsList = dataFromDB
+        //        }
+        
+        getItems()
         
         tableView.delegate = self
     }
@@ -26,7 +23,6 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemsList.count
     }
-    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
@@ -36,23 +32,21 @@ class TodoListViewController: UITableViewController {
         
         cell.textLabel?.text = item.title
         
-         cell.accessoryType =  item.done ? .checkmark : .none
-        
+        cell.accessoryType =  item.done ? .checkmark : .none
         
         return cell
     }
     
     
-    // method triggerd when select a cell
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //animate deselect
-        tableView.deselectRow(at: indexPath, animated: true)
-        //change accessoryType
         
+        tableView.deselectRow(at: indexPath, animated: true)
         
         itemsList[indexPath.row].done = !itemsList[indexPath.row].done
         
-        tableView.reloadData()
+        
+        self.saveItems()
     }
     
     
@@ -70,19 +64,46 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add", style: .default) { (action) in
             
             if let text = textField.text {
+                
                 self.itemsList.append(Item(text))
+                self.saveItems()
+                
             }
-            
-            
-            self.defaults.set(self.itemsList, forKey: "ToDoListArray")
-            
-            
-            self.tableView.reloadData()
         }
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    func saveItems (){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(self.itemsList)
+            try data.write(to: self.dataFilePath!)
+            self.tableView.reloadData()
+        }catch{
+            print(error)
+        }
+    }
+    
+    func getItems (){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do {
+                self.itemsList = try decoder.decode([Item].self, from: data)
+            }catch {
+                print(error)
+            }
+        }
+        
+        
+    }
+    
+    
+    func removeItem(index: Int){
+        itemsList.remove(at: index)
+        saveItems()
     }
 }
 
